d78fdc4ed7b12a12b60f2759f6570820c9dfc531
Walker de Alencar Oliveira <walkeralencar@gmail.com>
2013-06-14 16:36:14 -0400

    [UPDATE] - ajustes no README - ignorando bancos de dados
    [FIX]
    - fixed database problems

--- app.js
+++ app.js
@@ -11,19 +11,22 @@ var express = require('express')
   , fs = require('fs')
   , sqlite3 = require('sqlite3').verbose();
 
-fs.exists('data/database', function (exists) {
+fs.exists('data/database/wuml.sqlite3', function (exists) {
   console.info('Verifying database...');
-  var db = new sqlite3.Database('data/database');
+  var db = new sqlite3.Database('data/database/wuml.sqlite3');
 
   if (exists) {
     console.info('  Its Ok!');
   } else {
-    console.info('  Does not exist!');
-    console.info('  Creating database. This may take a while...');
-    fs.readFile('data/sql/create.sql', 'utf8', function (err, data) {
-      if (err) throw err;
-      db.exec(data, function (err) {
-        if (err) throw err;
+    console.info('  Does not exist! Creating database. This may take a while...');
+    fs.readFile('data/sql/create.sql', 'utf8', function (error, data) {
+      if (error) {
+        throw error;
+      }
+      db.exec(data, function (error) {
+        if (error) {
+          throw error;
+        }
         console.info('Done.');
       });
     });
