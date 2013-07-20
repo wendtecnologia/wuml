2fecaf8adc71cc525e734b4849f992504988313e
Walker de Alencar Oliveira <walkeralencar@gmail.com>
2013-07-20 14:48:28 -0400

    [ADD] - Template, jQueryMobile e alguns ajustes

--- app.js
+++ app.js
@@ -8,56 +8,43 @@ var express = require('express')
   , user = require('./routes/user')
   , http = require('http')
   , path = require('path')
-  , fs = require('fs')
-  , sqlite3 = require('sqlite3').verbose();
+  , config = require('./routes/config');
 
-fs.exists('data/database/wuml.sqlite3', function (exists) {
-  console.info('Verifying database...');
-  var db = new sqlite3.Database('data/database/wuml.sqlite3');
-
-  if (exists) {
-    console.info('  Its Ok!');
-  } else {
-    console.info('  Does not exist! Creating database. This may take a while...');
-    fs.readFile('data/sql/create.sql', 'utf8', function (error, data) {
-      if (error) {
-        throw error;
-      }
-      db.exec(data, function (error) {
-        if (error) {
-          throw error;
-        }
-        console.info('Done.');
-      });
-    });
-  }
-});
+//Verify Database
+config.checkDb();
 
 var app = express();
-
 // all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(express.cookieParser('your secret here'));
-app.use(express.session());
-app.use(app.router);
-app.use(require('stylus').middleware(__dirname + '/public'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.configure(function(){
+    app.set('port', process.env.PORT || 3000);
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'ejs');
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(express.cookieParser('your secret here'));
+    app.use(express.session());
+    app.use(app.router);
+    app.use(require('stylus').middleware(__dirname + '/public'));
+    app.use(express.static(path.join(__dirname, 'public')));
+});
 
 // development only
-if ('development' == app.get('env')) {
+app.configure('development', function(){
   app.use(express.errorHandler());
-}
+});
 
+//settings
+app.set('title','WUml v0.1');
+app.set('company','Wend');
+
+//routes
 app.get('/', routes.index);
 app.get('/users', user.list);
-app.get('/')
+app.get('');
 
+//server
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
